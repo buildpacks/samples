ac25a43d8808a1bd5734b276b139170d500a47d5
Andrew Meyer <meyeran@vmware.com>
2020-08-25 13:46:27 -0700

    Fix dependency name in plan.toml
    Signed-off-by: Andrew Meyer <meyeran@vmware.com>

--- buildpacks/dotnet-framework/bin/detect.bat
+++ buildpacks/dotnet-framework/bin/detect.bat
@@ -2,14 +2,12 @@
 
 set plan_path=%2
 
-:: TODO: Should this require/provide something meaningful?
-
 if exist c:\workspace\*.sln (
     echo [[requires]] >> %plan_path%
-    echo name = "io.buildpacks.samples.dotnet-framework" >> %plan_path%
+    echo name = "dotnet-framework" >> %plan_path%
 
     echo [[provides]] >> %plan_path%
-    echo name = "io.buildpacks.samples.dotnet-framework" >> %plan_path%
+    echo name = "dotnet-framework" >> %plan_path%
 
     echo Detection PASSED
     exit 0
