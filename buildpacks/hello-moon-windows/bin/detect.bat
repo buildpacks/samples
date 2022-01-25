59c7d0e44d495331dd18fae2517d1bbbace79ac0
Natalie Arellano <narellano@vmware.com>
2022-01-25 16:55:09 -0500

    Pass detect
    Signed-off-by: Natalie Arellano <narellano@vmware.com>

--- buildpacks/hello-moon-windows/bin/detect.bat
+++ buildpacks/hello-moon-windows/bin/detect.bat
@@ -4,13 +4,5 @@
 set plan_path=%2
 
 :: 2. DECLARE DEPENDENCIES (OPTIONAL)
-(
-echo [[requires]]
-echo name = "some-world"
-echo version = "0.1"
-
-echo [requires.metadata]
-echo world = "Earth-616"
-) >> %plan_path%
 
 exit /b 0
