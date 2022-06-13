18b8a2d12c13adada1def05567d5722812c2d53b
Javier Romero <rjavier@vmware.com>
2022-06-13 11:01:34 -0500

    Fix: CI: wine stack
    * Set default platform to amd64
    * Update base image to an LTS version (jammy)
    
    Signed-off-by: Javier Romero <rjavier@vmware.com>

--- stacks/build-stack.sh
+++ stacks/build-stack.sh
@@ -4,21 +4,27 @@ set -e
 ID_PREFIX="io.buildpacks.samples.stacks"
 
 DEFAULT_PREFIX=cnbs/sample-stack
+DEFAULT_PLATFORM=amd64
 
 REPO_PREFIX=${DEFAULT_PREFIX}
+PLATFORM=${DEFAULT_PLATFORM}
 
 usage() {
   echo "Usage: "
-  echo "  $0 [-p <prefix>] <dir>"
-  echo "    -p    prefix to use for images      (default: ${DEFAULT_PREFIX})"
+  echo "  $0 [-f <prefix>] [-p <platform>] <dir>"
+  echo "    -f    prefix to use for images      (default: ${DEFAULT_PREFIX})"
+  echo "    -p    prefix to use for images      (default: ${DEFAULT_PLATFORM})"
   echo "   <dir>  directory of stack to build"
   exit 1; 
 }
 
 while getopts "v:p:" o; do
   case "${o}" in
-    p)
+    f)
       REPO_PREFIX=${OPTARG}
+      ;;
+    p)
+      PLATFORM=${OPTARG}
       ;;
     \?)
       echo "Invalid option: -$OPTARG" 1>&2
@@ -55,14 +61,14 @@ RUN_IMAGE=${REPO_PREFIX}-run:${TAG}
 BUILD_IMAGE=${REPO_PREFIX}-build:${TAG}
 
 if [[ -d "${IMAGE_DIR}/base" ]]; then
-  docker build -t "${BASE_IMAGE}" "${IMAGE_DIR}/base"
+  docker build --platform=${PLATFORM} -t "${BASE_IMAGE}" "${IMAGE_DIR}/base"
 fi
 
 echo "BUILDING ${BUILD_IMAGE}..."
-docker build --build-arg "base_image=${BASE_IMAGE}" --build-arg "stack_id=${STACK_ID}" -t "${BUILD_IMAGE}"  "${IMAGE_DIR}/build"
+docker build --platform=${PLATFORM} --build-arg "base_image=${BASE_IMAGE}" --build-arg "stack_id=${STACK_ID}" -t "${BUILD_IMAGE}"  "${IMAGE_DIR}/build"
 
 echo "BUILDING ${RUN_IMAGE}..."
-docker build --build-arg "base_image=${BASE_IMAGE}" --build-arg "stack_id=${STACK_ID}" -t "${RUN_IMAGE}" "${IMAGE_DIR}/run"
+docker build --platform=${PLATFORM} --build-arg "base_image=${BASE_IMAGE}" --build-arg "stack_id=${STACK_ID}" -t "${RUN_IMAGE}" "${IMAGE_DIR}/run"
 
 echo
 echo "STACK BUILT!"
