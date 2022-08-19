b45c69e204a300e7b6c8379b564be05b862c06e3
Sambo Chea <sombochea@cubetiqs.com>
2022-08-19 16:03:44 +0700

    Upgrade Spring Boot to 2.7.3
    Signed-off-by: Sambo Chea <sombochea@cubetiqs.com>

--- apps/kotlin-gradle/build.gradle.kts
+++ apps/kotlin-gradle/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.2"
+	id("org.springframework.boot") version "2.7.3"
 	id("io.spring.dependency-management") version "1.0.13.RELEASE"
 	kotlin("jvm") version "1.7.10"
 	kotlin("plugin.spring") version "1.7.10"
